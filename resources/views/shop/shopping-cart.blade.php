3e6f3dfda0d73f367a5792d2ebb0f240584cf994
gil <blackdem17@hotmail.com>
2017-04-10 13:17:36 -0500

    class for shopping_cart created  and finished

--- resources/views/shop/shopping-cart.blade.php
+++ resources/views/shop/shopping-cart.blade.php
@@ -6,10 +6,16 @@
 
 @section('content')
   @if (Session::has('cart'))
-
+    <div class="row">
+      <div class="col-md-6 col-sm-6 col-md-offset-3 col-sm-offset-3">
+        <ul class="list-group">
+          @foreach ($products as $product)
+            <li>{{$product}}</li>
+          @endforeach
+        </ul>
+      </div>
+    </div>
   @else
-      @foreach ($products as $product)
 
-      @endforeach
   @endif
 @endsection
